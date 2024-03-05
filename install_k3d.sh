8eb6724367ae0f1677def76fd815ae8ea9660513
Raphael Damito Baldini <57991422+raphaelbaldini@users.noreply.github.com>
2024-03-05 09:34:45 -0300

    Identation code

--- install_k3d.sh
+++ install_k3d.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
 
 sudo apt-get update 
-
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do 
-sudo apt-get remove $pkg
+  sudo apt-get remove -y $pkg
 done
-
-sudo apt-get update
-
-sudo apt-get install ca-certificates curl gnupg -y
-
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
 echo \
 "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
 "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get autoremove -y
 sudo systemctl start docker
+sudo systemctl enable docker
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
